b593701a66a51d74d6e852f55319365da033b226
zahid-58 <1913133@stud.hs-mannheim.de>
2024-09-01 19:46:37 +0200

    Paralyzed v1

--- Paralyzed Watch App/View/StartStopView.swift
+++ Paralyzed Watch App/View/StartStopView.swift
@@ -32,10 +32,12 @@ struct StartStopView: View {
                     Button("Tap to activate") {
                         isActivated = true
 
-                        uploadRealmToDiscord()
+                        //uploadRealmToDiscord()
+                        //deleteRealmDatabase()
+                        //listFilesInDocumentsDirectory()
 
-//                        timerManager.startCycle()
-//                        audioActive = true
+                        timerManager.startCycle()
+                        audioActive = true
                         
                     }
                     .buttonStyle(.borderedProminent)
@@ -130,68 +132,121 @@ struct StartStopView: View {
         }
     }
     
-    func uploadRealmToDiscord() {
-        let fileManager = FileManager.default
-        
-        // Pfad zum Documents-Verzeichnis abrufen
-        let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
-        
-        // Pfad zur default.realm Datei
-        let realmFileURL = documentsDirectory.appendingPathComponent("default.realm")
-        
-        // Überprüfen, ob die Datei existiert
-        guard fileManager.fileExists(atPath: realmFileURL.path) else {
-            print("Die Datei default.realm existiert nicht.")
-            return
-        }
-        
-        // URL des Discord Webhooks
-        let webhookURL = URL(string: "https://discord.com/api/webhooks/1223418868467499199/UOAUGjmezHHkJNbKp0yRfX7M4Bu9Fk5gjYmFy_e-pydLym1lLWwg0blPEN12RZ5ETadi")!
-        
-        // Dateiinhalt lesen
-        do {
-            let fileData = try Data(contentsOf: realmFileURL)
-            
-            // Multipart-Formdaten erstellen
-            var request = URLRequest(url: webhookURL)
-            request.httpMethod = "POST"
-            
-            let boundary = UUID().uuidString
-            request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-            
-            var body = Data()
-            
-            body.append("--\(boundary)\r\n".data(using: .utf8)!)
-            body.append("Content-Disposition: form-data; name=\"file\"; filename=\"default.realm\"\r\n".data(using: .utf8)!)
-            body.append("Content-Type: application/octet-stream\r\n\r\n".data(using: .utf8)!)
-            body.append(fileData)
-            body.append("\r\n".data(using: .utf8)!)
-            body.append("--\(boundary)--\r\n".data(using: .utf8)!)
-            
-            request.httpBody = body
-            
-            // Datei an Discord senden
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                if let error = error {
-                    print("Fehler beim Hochladen der Datei: \(error.localizedDescription)")
-                    return
-                }
-                
-                guard let response = response as? HTTPURLResponse, response.statusCode == 204 else {
-                    print("Fehlerhafte Antwort vom Server")
-                    return
-                }
-                
-                print("Datei erfolgreich hochgeladen!")
-            }
-            
-            task.resume()
-            
-        } catch {
-            print("Fehler beim Lesen der Datei: \(error.localizedDescription)")
-        }
-    }
-    
+//    func uploadRealmToDiscord() {
+//        let fileManager = FileManager.default
+//        
+//        // Pfad zum Documents-Verzeichnis abrufen
+//        let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+//        
+//        // Pfad zur default.realm Datei
+//        let realmFileURL = documentsDirectory.appendingPathComponent("default.realm")
+//        
+//        // Überprüfen, ob die Datei existiert
+//        guard fileManager.fileExists(atPath: realmFileURL.path) else {
+//            print("Die Datei default.realm existiert nicht.")
+//            return
+//        }
+//        
+//        // URL des Discord Webhooks
+//        let webhookURL = URL(string: "https://discord.com/api/webhooks/1223418868467499199/UOAUGjmezHHkJNbKp0yRfX7M4Bu9Fk5gjYmFy_e-pydLym1lLWwg0blPEN12RZ5ETadi")!
+//        
+//        // Dateiinhalt lesen
+//        do {
+//            let fileData = try Data(contentsOf: realmFileURL)
+//            
+//            // Multipart-Formdaten erstellen
+//            var request = URLRequest(url: webhookURL)
+//            request.httpMethod = "POST"
+//            
+//            let boundary = UUID().uuidString
+//            request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+//            
+//            var body = Data()
+//            
+//            body.append("--\(boundary)\r\n".data(using: .utf8)!)
+//            body.append("Content-Disposition: form-data; name=\"file\"; filename=\"default.realm\"\r\n".data(using: .utf8)!)
+//            body.append("Content-Type: application/octet-stream\r\n\r\n".data(using: .utf8)!)
+//            body.append(fileData)
+//            body.append("\r\n".data(using: .utf8)!)
+//            body.append("--\(boundary)--\r\n".data(using: .utf8)!)
+//            
+//            request.httpBody = body
+//            
+//            // Datei an Discord senden
+//            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//                if let error = error {
+//                    print("Fehler beim Hochladen der Datei: \(error.localizedDescription)")
+//                    return
+//                }
+//                
+//                guard let response = response as? HTTPURLResponse, response.statusCode == 204 else {
+//                    print("Fehlerhafte Antwort vom Server")
+//                    return
+//                }
+//                
+//                print("Datei erfolgreich hochgeladen!")
+//            }
+//            
+//            task.resume()
+//            
+//        } catch {
+//            print("Fehler beim Lesen der Datei: \(error.localizedDescription)")
+//        }
+//    }
+//    
+//    func listFilesInDocumentsDirectory() {
+//         let fileManager = FileManager.default
+//         
+//         // Pfad zum Documents-Verzeichnis abrufen
+//         let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+//         
+//         do {
+//             // Inhalte des Documents-Verzeichnisses auflisten
+//             let fileURLs = try fileManager.contentsOfDirectory(at: documentsDirectory, includingPropertiesForKeys: nil)
+//             
+//             print("Inhalt des Documents-Verzeichnisses:")
+//             for fileURL in fileURLs {
+//                 print(fileURL.lastPathComponent)
+//             }
+//         } catch {
+//             print("Fehler beim Auflisten der Dateien im Documents-Verzeichnis: \(error)")
+//         }
+//     }
+//    
+//    func deleteRealmDatabase() {
+//        let fileManager = FileManager.default
+//
+//        // Pfad zum Dokumentenverzeichnis abrufen
+//        guard let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+//            print("Dokumentenverzeichnis nicht gefunden.")
+//            return
+//        }
+//
+//        // Erstellen eines Arrays mit den zu löschenden Dateinamen
+//        let realmFiles = [
+//            "default.realm",
+//            "default.realm.management",
+//            "default.realm.lock",
+//            "default.realm.note"
+//        ]
+//
+//        // Durchlaufen und Löschen der Dateien
+//        for fileName in realmFiles {
+//            let fileURL = documentsDirectory.appendingPathComponent(fileName)
+//            
+//            do {
+//                if fileManager.fileExists(atPath: fileURL.path) {
+//                    try fileManager.removeItem(at: fileURL)
+//                    print("Datei \(fileName) erfolgreich gelöscht.")
+//                } else {
+//                    print("Datei \(fileName) existiert nicht.")
+//                }
+//            } catch {
+//                print("Fehler beim Löschen der Datei \(fileName): \(error.localizedDescription)")
+//            }
+//        }
+//    }
+
     
 }
 
